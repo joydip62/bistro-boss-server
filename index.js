880b1d1d575ed570290813b145c8eeb7aae43d5e
Stefany Cardozo <joydipbanik6@gmail.com>
2023-11-19 14:19:36 +0530

    making verify token and check admin or not api

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 
@@ -33,6 +35,43 @@ async function run() {
     const reviewsCollection = database.collection("reviews");
     const cartsCollection = database.collection("carts");
 
+    // middleware
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'forbidden access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "forbidden access" });
+        } 
+        res.decoded = decoded;
+        next();
+      });
+    }
+    // check admin or not
+    app.get('/user/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== res.decoded.email) {
+        return res.status(403).send({message: 'unauthorized access'})
+      }
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
+      
+    })
+    // jwt related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: '1h'
+      });
+      res.send({token})
+    })
     // user api
     app.get("/users", async (req, res) => {
       const result = await userCollection.find().toArray();
