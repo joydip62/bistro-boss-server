a609413682156a8e78d054f514f2ed2421a2af52
Stefany Cardozo <joydipbanik6@gmail.com>
2023-11-21 12:54:54 +0530

    making menu delete api

--- index.js
+++ index.js
@@ -157,6 +157,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/menu/:id", verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    })
+
     //   reviews api
     app.get("/reviews", async (req, res) => {
       const result = await reviewsCollection.find().toArray();
