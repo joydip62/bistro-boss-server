2555672459ab8df27955eaff4577f5197ba7de72
Stefany Cardozo <joydipbanik6@gmail.com>
2023-11-23 16:38:25 +0530

    making chart

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 require("dotenv").config();
 
 // strip payment
-const stripe = require('stripe')(process.env.STRIP_SECRET_KEY)
+const stripe = require("stripe")(process.env.STRIP_SECRET_KEY);
 // mongodb
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jucoem4.mongodb.net/?retryWrites=true&w=majority`;
@@ -48,19 +48,19 @@ async function run() {
     });
 
     // middleware
- const verifyToken = (req, res, next) => {
-   if (!req.headers.authorization) {
-     return res.status(401).send({ message: "unauthorized access" });
-   }
-   const token = req.headers.authorization.split(" ")[1];
-   jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
-     if (err) {
-       return res.status(401).send({ message: "unauthorized access" });
-     }
-     req.decoded = decoded;
-     next();
-   });
- };
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
 
     // use verify admin after verifyToken
     const verifyAdmin = async (req, res, next) => {
@@ -74,25 +74,91 @@ async function run() {
       next();
     };
 
+    // admin related route
+
+    // admin dashboard
+    app.get("/admin-stats", verifyToken, verifyAdmin, async (req, res) => {
+      const users = await userCollection.estimatedDocumentCount();
+      const menuItem = await menuCollection.estimatedDocumentCount();
+      const order = await paymentCollection.estimatedDocumentCount();
+
+      // for revenue with aggregate
+      const result = await paymentCollection
+        .aggregate([
+          {
+            $group: {
+              _id: null,
+              totalRevenue: {
+                $sum: "$price",
+              },
+            },
+          },
+        ])
+        .toArray();
+      const revenue = result.length > 0 ? result[0].totalRevenue : 0;
+      res.send({
+        users,
+        menuItem,
+        order,
+        revenue,
+      });
+    });
+
+    // admin chart
+    app.get("/order-stats", verifyToken, verifyAdmin, async (req, res) => {
+      const result = await paymentCollection
+        .aggregate([
+          {
+            $unwind: "$menuItemIds",
+          },
+          {
+            $lookup: {
+              from: "menu",
+              localField: "menuItemIds",
+              foreignField: "_id",
+              as: "menuItems",
+            },
+          },
+          {
+            $unwind: "$menuItems",
+          },
+          {
+            $group: {
+              _id: "$menuItems.category",
+              quantity: { $sum: 1 },
+              totalRevenue: { $sum: "$menuItems.price" },
+            },
+          },
+          {
+            $project: {
+              _id: 0,
+              category: "$_id",
+              quantity: "$quantity",
+              totalRevenue: "$totalRevenue",
+            },
+          },
+        ])
+        .toArray();
+
+      res.send(result);
+    });
+
     // check admin or not
-    app.get(
-      "/users/admin/:email",
-      verifyToken,
-      verifyAdmin,
-      async (req, res) => {
-        const email = req.params.email;
-        if (email !== res.decoded.email) {
-          return res.status(403).send({ message: "forbidden access" });
-        }
-        const query = { email: email };
-        const user = await userCollection.findOne(query);
-        let admin = false;
-        if (user) {
-          admin = user?.role === "admin";
-        }
-        res.send({ admin });
+    app.get("/users/admin/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "forbidden access" });
       }
-    );
+
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === "admin";
+      }
+      res.send({ admin });
+    });
 
     // user api
     app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
